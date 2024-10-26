8d9d84f9b5b782ddb7a3cf8453e8d7e6fe98cb9c
Christopher Pratt <christophernathanpratt@gmail.com>
2024-10-26 17:37:34 -0400

    add callouts when navigating using the flatlist cards

--- features/finder-display/components/MapMarkers.jsx
+++ features/finder-display/components/MapMarkers.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Marker } from "react-native-maps";
 
 const MapMarkers = ({ items, selectedMarker, onMarkerSelect = () => {} }) => {
   return items.map((item) => {
-    const isSelected = item.id === selectedMarker;
-    const color = isSelected ? "blue" : "green";
-    const title = item.species;
-    const zIndex = isSelected ? 100 : 0;
-
     return (
-      <Marker
-        key={item.id}
-        title={title}
-        coordinate={{ latitude: item.lat, longitude: item.lon }}
-        pinColor={color}
-        style={{ zIndex: zIndex }}
-        onSelect={() => onMarkerSelect(item.id)}
+      <MapMarker
+        item={item}
+        selectedMarker={selectedMarker}
+        onMarkerSelect={onMarkerSelect}
       />
     );
   });
+};
+
+const MapMarker = ({ item, selectedMarker, onMarkerSelect }) => {
+  const ref = useRef(null);
+
+  const isSelected = item.id === selectedMarker;
+  const color = isSelected ? "blue" : "green";
+  const title = item.species;
+  const zIndex = isSelected ? 100 : 0;
+
+  useEffect(() => {
+    if (isSelected && ref.current) {
+      ref.current.showCallout();
+    }
+  }, [isSelected]);
+
+  return (
+    <Marker
+      ref={ref}
+      key={item.id}
+      title={title}
+      coordinate={{ latitude: item.lat, longitude: item.lon }}
+      pinColor={color}
+      style={{ zIndex: zIndex }}
+      onSelect={() => onMarkerSelect(item.id)}
+    />
+  );
 };
 
 export default MapMarkers;
